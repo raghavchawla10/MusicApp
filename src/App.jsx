ea3c9c2ab7d198b2c933195f6f7ec226d33af0b0
raghavchawla10 <raghavchawla200@gmail.com>
2026-04-06 12:40:12 +0530

    added search menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./Components/About";
 import GenresPage from "./Components/GenresPage";
 import SongsPage from "./Components/SongsPage";
 import SongDetail from "./Components/SongDetail";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -12,6 +13,23 @@ function App() {
       <div className="header">
         <Navbar />
       </div>
+      <Link to="/songs" style={{ textDecoration: "none", color: "#00f0ff" }}>
+  <div style={{ margin: "20px", display: "flex", gap: "10px", alignItems: "center" , backgroundColor : "pink"}}>
+    <span>🎵</span>
+    <input 
+      type="text" 
+      placeholder="Type your song name" 
+      style={{ 
+        padding: "8px", 
+        borderRadius: "6px", 
+        border: "1px solid #00f0ff", 
+        background: "black", 
+        color: "#00f0ff" 
+      }} 
+    />
+  </div>
+</Link>
+
       <div className="body">
         <Routes>
           <Route path="/" element={<GenresPage />} />
